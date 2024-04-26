a3e10f542dc5bef5e6532b8277ce904c3d9f594d
Abinand P <abinand0911@gmail.com>
2024-04-26 12:05:45 +0530

    Completed Responsiveness in the home page

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -3,8 +3,8 @@ import React from "react";
 const Layout = ({ children, className = "" }) => {
   return (
     <div
-      className={` w-full h-full inline-block z-0 bg-light p-32 dark:bg-dark 
-      xl:p-25 lg:p-18 md:p-17 sm:p-8
+      className={` w-full h-full inline-block z-1 p-32 dark:bg-dark 
+      xl:p-25 lg:p-18 md:p-17 sm:p-8 xs:py-0 bg-ligth
        ${className}`}
     >
       {children}

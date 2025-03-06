668acc4efa2e730b13a041a6dca68dc5cf1b2d15
Abinand P <abinand0911@gmail.com>
2025-03-06 10:40:55 +0530

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -32,15 +32,13 @@ export default function Home() {
             items-center self-center"
             >
               <AnimatedText
-                text="Turning Vision Into Reality With Code And Design"
+                text="Turning Vision Into Reality With Code And Problem Solving"
                 className="!text-6xl xl:!text-5xl lg:!text-center lg:!text-6xl  md:!text-5xl sm:!text-3xl lg:w-full xs:!text-4xl
               !text-left"
               />
               <p className="my-4 text-base font-medium md:text-small sm:text-xs xs:text-sm">
                 As a skilled full-stack developer, I am dedicated to turning
-                ideas into innovative web applications. Explore my latest
-                projects and articles, showcasing my expertise in web
-                development and mircroservices.
+                ideas into innovative web applications. Trying to improve the knowledge and exploring the world of Web development and Software development best practices.
               </p>
 
               <div

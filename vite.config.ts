ef59b7a0e2eeb172df81f4b96c53e5088205f28c
Abinand P <abinand0911@gmail.com>
2025-11-01 21:51:26 +0530

    fix: deployment
    Signed-off-by: Abinand P <abinand0911@gmail.com>

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,14 @@ export default defineConfig({
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
-    emptyOutDir: true
+    emptyOutDir: true,
+    rollupOptions: {
+      output: {
+        entryFileNames: 'assets/[name]-[hash].js',
+        chunkFileNames: 'assets/[name]-[hash].js',
+        assetFileNames: 'assets/[name]-[hash][extname]'
+      }
+    }
   },
   server: {
     port: 3000
